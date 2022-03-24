cd1d49bac78fd8739082e636160ba529ade2ce12
jacobbra <jacobbra@kth.se>
2022-03-24 13:05:11 +0100

    Added comments

--- lab2/skeleton.cpp
+++ lab2/skeleton.cpp
@@ -39,38 +39,50 @@ bool CheckTriangleIntersection(
 	Intersection& intersection
 )
 {
+	//Get the axises of the triangles.
 	vec3 t1 = triangle.v1 - triangle.v0;
 	vec3 t2 = triangle.v2 - triangle.v0;
 
-	vec3 normal_ = triangle.normal;
-	vec3 normalized = normal_ / glm::length(normal_);
-
-	float d = glm::dot(dir, normalized);
+	float d = glm::dot(dir, triangle.normal);
 	float epsilon = 0.0001f;
 
+	//Make sure that the ray isn't parallel to surface within an epsilon.
 	if (fabs(d) < epsilon) {
 		return false;
 	}
 
-	float t = glm::dot((triangle.v0 - start), normalized);
+	//Calculate intersection
+	float inter = glm::dot((triangle.v0 - start), triangle.normal);
+	float t = inter / d;
 
-	float lambda = t / d;
-	if (lambda < 0 || lambda + epsilon > 1/*maxLambda?*/) {
+	//Check that the intersection isn't too close or too far away
+	if (t < 0 || t + epsilon > 1/*maxLambda?*/) {
 		return false;
 	}
 
-	vec3 p = start + lambda * dir;
-	double area = glm::length(normal_) / 2.0;
+	//Calculate point of intersection
+	vec3 p = start + t * dir;
+	
+	//### Cramer's rule ###
+	//Area of triangle
+	double area = glm::length(triangle.normal) / 2.0;
 
-	double beta = glm::dot(glm::cross(t1, p - triangle.v0), normal_) / glm::dot(normal_, normal_);
+	//Area of beta part of the triangle. Check that it is within parameter, otherwise we end early.
+	double beta = glm::dot(glm::cross(t1, p - triangle.v0), triangle.normal) / glm::dot(triangle.normal, triangle.normal);
 	if (!(beta <= 1 && beta >= 0)) return false;
-	double gamma = glm::dot(glm::cross(p - triangle.v0, t2), normal_) / glm::dot(normal_, normal_);
+
+	//Area of gamma part of the triangle. Check that it is within parameter, otherwise we end early.
+	double gamma = glm::dot(glm::cross(p - triangle.v0, t2), triangle.normal) / glm::dot(triangle.normal, triangle.normal);
 	if (!(gamma <= 1 && gamma >= 0)) return false;
+
+	//Area of gamma part of the triangle. Check that it is within parameter, otherwise we end early.
 	double alpha = 1 - gamma - beta;
 	if (!(alpha <= 1 && alpha >= 0)) return false;
 
+	//Put into intersection
 	intersection.distance = glm::length(p);
 	intersection.position = p;
+	//NOTE: We don't set index here, we set it when it returns.
 	intersection.triangleIndex = 0;
 	return true;
 }
@@ -82,17 +94,21 @@ bool ClosestIntersection(
 	Intersection& closestIntersection
 )
 {
+	//Assume there isn't any intersections
 	bool hasIntersection = false;
 	Intersection intersection;
 	float smallestDistance = std::numeric_limits<float>::max();
 
+	//Go through all the triangles and check if it is the closest intersection.
 	for (int i = 0; i < triangles.size(); i++)
 	{
 		if (CheckTriangleIntersection(start, dir, triangles[i], intersection))
 		{
+			//We have found an intersection
 			hasIntersection = true;
 			if (intersection.distance < smallestDistance)
 			{
+				//If it is closer than the last one we checked, save it and assign its triangle index.
 				smallestDistance = intersection.distance;
 				closestIntersection = intersection;
 				closestIntersection.triangleIndex = i;
