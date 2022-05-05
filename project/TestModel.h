f98b757c06493e73aa2092c83f61644b046313e2
jacobbra <jacobbra@kth.se>
2022-05-05 10:04:12 +0200

    IT WORKS!

--- project/TestModel.h
+++ project/TestModel.h
@@ -5,6 +5,7 @@
 
 #include <glm/glm.hpp>
 #include <vector>
+#include <iostream>
 
 // Used to describe a triangular surface:
 class Triangle
@@ -12,10 +13,8 @@ class Triangle
 public:
 	std::vector<glm::vec3> v;
 	glm::vec3 normal;
-	glm::vec3 color;
 
-	Triangle( glm::vec3 v0, glm::vec3 v1, glm::vec3 v2, glm::vec3 color )
-		: color(color)
+	Triangle( glm::vec3 v0, glm::vec3 v1, glm::vec3 v2)
 	{
 		v.push_back(v0);
 		v.push_back(v1);
@@ -34,13 +33,110 @@ public:
 struct Object {
 	std::vector<Triangle> triangles;
 	glm::vec3 colour;
+	int offset = 0;
 };
+
+struct Boid {
+	glm::vec3 pos;
+	glm::vec3 rot;
+	Object* mesh;
+
+	Boid(const glm::vec3& p, const glm::vec3& r, Object* obj) {
+		pos = p;
+		rot = r;
+		mesh = obj;
+	}
+
+	void rotate(glm::vec3 r) {
+		rot += r;
+	}
+
+	void move(const glm::vec3& p) {
+		pos += p;
+	}
+
+	glm::mat4 getModel() {
+		glm::mat4 model = glm::mat4(1.0f)*glm::eulerAngleYXZ(rot[1], rot[0], rot[2]);
+		model *= glm::translate(model, pos);
+		return model;
+	}
+};
+
+void LoadLevel(std::vector<Object>& objects, std::vector<Boid>& boids)
+{
+	using glm::vec3;
+
+	// Defines colors:
+	vec3 red(0.75f, 0.15f, 0.15f);
+	vec3 yellow(0.75f, 0.75f, 0.15f);
+	vec3 green(0.15f, 0.75f, 0.15f);
+	vec3 cyan(0.15f, 0.75f, 0.75f);
+	vec3 blue(0.15f, 0.15f, 0.75f);
+	vec3 purple(0.75f, 0.15f, 0.75f);
+	vec3 white(0.75f, 0.75f, 0.75f);
+
+	objects.clear();
+	objects.resize(1);
+
+	boids.clear();
+	const int numBoids = 2;
+	boids.reserve(numBoids);
+
+	// ---------------------------------------------------------------------------
+	// Room
+
+	float L = 10;			// Length of Cornell Box side.
+
+	
+	vec3 M(1.0f / 2.0f, glm::sqrt(2.0f / 3.0f) / 2.0f, 1.0f / (2.0f * glm::sqrt(3.0f)));
+	vec3 A = vec3(0, 0, 0) - M;
+	vec3 B = vec3(1, 0, 0) - M;
+	vec3 C = vec3(0.5, 0, glm::sqrt(3.0f) / 2.0f) - M;
+	vec3 D = vec3(0.5, glm::sqrt(2.0f / 3.0f), 1.0f / (2.0f * glm::sqrt(3.0f))) - M;
+	
+	objects[0].colour = red;
+	objects[0].offset = 0;
+	objects[0].triangles.push_back(Triangle(A, B, C));
+	objects[0].triangles.push_back(Triangle(A, C, D));
+	objects[0].triangles.push_back(Triangle(B, C, D));
+	objects[0].triangles.push_back(Triangle(A, B, D));
+
+	// ----------------------------------------------
+	// Scale to the volume [-1,1]^3
+
+	for (size_t j = 0; j < objects.size(); ++j)
+	{
+		std::vector<Triangle>& triangles = objects[j].triangles;
+		for (int i = 0; i < triangles.size(); i++)
+		{
+
+			triangles[i].v[0] *= 2 / L;
+			triangles[i].v[1] *= 2 / L;
+			triangles[i].v[2] *= 2 / L;
+
+			triangles[i].ComputeNormal();
+		}
+
+	}
+
+	boids.push_back(Boid(
+		vec3(0, 0, 0), 
+		vec3(0, 0, 0), 
+		&objects[0])
+	);
+
+	boids.push_back(Boid(
+		vec3(0.5, 0.5, 0.5),
+		vec3(0, 0, 0),
+		&objects[0])
+	);
+}
 
 // Loads the Cornell Box. It is scaled to fill the volume:
 // -1 <= x <= +1
 // -1 <= y <= +1
 // -1 <= z <= +1
-void LoadTestModel( std::vector<Object>& objects )
+/*void LoadTestModel( std::vector<Object>& objects )
 {
 	using glm::vec3;
 
@@ -193,6 +289,6 @@ void LoadTestModel( std::vector<Object>& objects )
 		}
 		
 	}
-}
+}*/
 
 #endif
--- project/skeleton.cpp
+++ project/skeleton.cpp
@@ -8,6 +8,7 @@
 #include <glm/gtx/constants.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/euler_angles.hpp>
 #include "TestModel.h"
 #include "glad.h"
 
@@ -40,6 +41,7 @@ const int SCREEN_HEIGHT = 1000;
 SDL_Surface* screen;
 int t;
 vector<Object> objects;
+vector<Boid> boids;
 vec4 cameraPos(0, 0, -3.001, 1);
 mat4 cameraMatrix(1);
 mat4 projectionMatrix = glm::perspective(53.0f,
@@ -69,6 +71,7 @@ void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result);
 void Interpolate(Pixel a, Pixel b, vector<Pixel>& result);
 void VertexShader(const Vertex& v, Pixel& p);
 void PixelShader(const Pixel& p);
+
 
 
 
@@ -157,7 +160,7 @@ void setupShaders()
 
 int main(int argc, char* argv[])
 {
-	LoadTestModel(objects);
+	LoadLevel(objects, boids);
 	screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
@@ -191,7 +194,7 @@ int main(int argc, char* argv[])
 				}
 			}
 
-			cout << triangles[i].normal[0] << " " << triangles[i].normal[1] << " " << triangles[i].normal[2] << endl;
+			//cout << triangles[i].normal[0] << " " << triangles[i].normal[1] << " " << triangles[i].normal[2] << endl;
 		}
 		curObjOffset += triangles.size();
 	}
@@ -253,18 +256,18 @@ void handleInput(){
 	if (keystate[SDLK_UP])
 	{
 		// Move camera forward
-		cameraPos += 0.001f * forward;
+		cameraPos += 0.001f * vec4(0, 0, 1, 0);
 	}
 	if (keystate[SDLK_DOWN])
 	{
 		// Move camera backward
-		cameraPos -= 0.001f * forward;
+		cameraPos -= 0.001f * vec4(0, 0, 1, 0);;
 	}
 	if (keystate[SDLK_LEFT])
 	{
 		// Move camera to the left
 		//cameraPos[0] -= 0.1;
-		yaw += 0.05f;
+		yaw -= 0.05f;
 		float rad = glm::radians(yaw);
 		R = mat4(glm::cos(rad), 0, glm::sin(rad), 0,
 			0, 1, 0, 0,
@@ -275,7 +278,7 @@ void handleInput(){
 	{
 		// Move camera to the right
 		//cameraPos[0] += 0.1;
-		yaw -= 0.05f;
+		yaw += 0.05f;
 		float rad = glm::radians(yaw);
 		R = mat4(glm::cos(rad), 0, glm::sin(rad), 0,
 			0, 1, 0, 0, 
@@ -287,12 +290,12 @@ void handleInput(){
 	vec3 r(right);
 	vec3 d(down);
 
-	if (keystate[SDLK_w]) lightPos += 0.001f * f;
-	if (keystate[SDLK_s]) lightPos -= 0.001f * f;
+	if (keystate[SDLK_w]) lightPos -= 0.001f * f;
+	if (keystate[SDLK_s]) lightPos += 0.001f * f;
 	if (keystate[SDLK_d]) lightPos += 0.001f * r;
 	if (keystate[SDLK_a]) lightPos -= 0.001f * r;
-	if (keystate[SDLK_q]) lightPos += 0.001f * d;
-	if (keystate[SDLK_e]) lightPos -= 0.001f * d;
+	if (keystate[SDLK_q]) lightPos -= 0.001f * d;
+	if (keystate[SDLK_e]) lightPos += 0.001f * d;
 
 	if( keystate[SDLK_RSHIFT] )
 		;
@@ -317,253 +320,7 @@ void Update()
 	handleInput();
 }
 
-/*
-void ComputePolygonRows(const vector<Pixel>& vertexPixels, vector<Pixel>& leftPixels,
-	vector<Pixel>& rightPixels)
-{
-	// 1. Find max and min y-value of the polygon
-	// and compute the number of rows it occupies.
-
-	// 2. Resize leftPixels and rightPixels
-	// so that they have an element for each row.
-
-	// 3. Initialize the x-coordinates in leftPixels
-	// to some really large value and the x-coordinates
-	// in rightPixels to some really small value.
-
-	// 4. Loop through all edges of the polygon and use
-	// linear interpolation to find the x-coordinate for
-	// each row it occupies. Update the corresponding
-	// values in rightPixels and leftPixels.
-
-	int min = numeric_limits<int>::max();
-	int max = -numeric_limits<int>::max();
-
-	for (int i = 0; i < vertexPixels.size(); i++)
-	{
-		if (min > vertexPixels[i].y) min = vertexPixels[i].y;
-		if (max < vertexPixels[i].y) max = vertexPixels[i].y;
-	}
-	int rows = max-min+1;
-
-	leftPixels.resize(rows);
-	rightPixels.resize(rows);
-
-	for (int i = 0; i < rows; ++i)
-	{
-		leftPixels[i].x = +numeric_limits<int>::max();
-		rightPixels[i].x = -numeric_limits<int>::max();
-		leftPixels[i].y = i + min;
-		rightPixels[i].y = i + min;
-	}
-
-	for (int i = 0; i < vertexPixels.size(); i++)
-	{
-		Pixel a = vertexPixels[i];
-		Pixel b = vertexPixels[(i+1) % vertexPixels.size()];
-
-		Pixel delta;
-		delta.x = glm::abs(a.x - b.x);
-		delta.y = glm::abs(a.y - b.y);
-		int pixels = glm::max(delta.x, delta.y) + 1;
-
-		vector<Pixel> line(pixels);
-		Interpolate(a, b, line);
-		for (int j = 0; j < line.size(); j++)
-		{
-			int y = line[j].y - min;
-			if (line[j].x < leftPixels[y].x) leftPixels[y] = line[j];
-			if (line[j].x > rightPixels[y].x) rightPixels[y] = line[j];
-		}
-	}
-}
-
-void DrawRows(const vector<Pixel>& leftPixels, const vector<Pixel>& rightPixels){
-	for(int i = 0; i < leftPixels.size(); i++){
-		int rowLength = rightPixels[i].x - leftPixels[i].x + 1;
-
-		vector<Pixel> rowPixel(rowLength);
-		Interpolate(leftPixels[i], rightPixels[i], rowPixel);
-
-		for(int j = 0; j < rowLength; j++){
-			PixelShader(rowPixel[j]);
-		}
-	}
-}
-
-void DrawPolygon( const vector<Vertex>& vertices ){
-	int V = vertices.size();
-
-	//Project the corners
-	vector<Pixel> vertexPixels(V);
-	for(int i = 0; i < V; ++i) VertexShader(vertices[i], vertexPixels[i]);
-
-	//Compute the rows of the polygon
-	vector<Pixel> leftPixels;
-	vector<Pixel> rightPixels;
-	ComputePolygonRows( vertexPixels, leftPixels, rightPixels );
-
-	//Draw the polygon
-	DrawRows(leftPixels, rightPixels);
-}
-
-void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result)
-{
-	int N = result.size();
-	glm::vec2 step = glm::vec2(b - a) / float(glm::max(N - 1, 1));
-	glm::vec2 current(a);
-	for (int i = 0; i < N; ++i)
-	{
-		result[i] = glm::round(current);
-		current += step;
-	}
-}
-
-void Interpolate(Pixel a, Pixel b, vector<Pixel>& result){
-	int N = result.size();
-
-	//Convert the pixels to float vectors in 3d space
-	glm::vec3 aVec(a.x, a.y, a.zinv);
-	glm::vec3 bVec(b.x, b.y, b.zinv);
-
-	//glm::vec3 aColour = a.illumination;
-	//glm::vec3 bColour = b.illumination;
-	
-	glm::vec3 aPos3d = a.pos3d * a.zinv;
-	glm::vec3 bPos3d = b.pos3d * b.zinv;
-
-	glm::vec3 step = (bVec - aVec) / float(glm::max(N - 1, 1));
-	//glm::vec3 colourStep = (bColour - aColour) / float(glm::max(N - 1, 1));
-	//glm::vec3 pos3dStep = (bPos3d * b.zinv - aPos3d * a.zinv) / float(glm::max(N - 1, 1));
-	glm::vec3 pos3dStep = (bPos3d - aPos3d) / float(glm::max(N - 1, 1));
-
-	glm::vec3 current(aVec);
-	//glm::vec3 currentColour(aColour);
-	glm::vec3 currentPos3d(aPos3d);
-	for (int i = 0; i < N; ++i)
-	{
-		//Convert the result back to a pixel
-		Pixel res;
-		res.x = glm::round(current.x);
-		res.y = glm::round(current.y);
-		res.zinv = current.z;
-		//res.illumination = currentColour;
-		res.pos3d = currentPos3d / res.zinv; //Pixel illumination
-		result[i] = res;
-
-		current += step;
-		//currentColour += colourStep;
-		currentPos3d += pos3dStep;
-	}
-}
-
-void VertexShader(const Vertex& v, Pixel& p)
-{
-	//Convert the 3D coordinate to camera coordinates
-	vec3 t = (v.position - cameraPos) * R;
-
-	p.x = focalLength * t.x / t.z + SCREEN_WIDTH / 2;
-	p.y = focalLength * t.y / t.z + SCREEN_HEIGHT / 2;
-	p.zinv = 1.0f / t.z; //Calculate the inverse of z for the depth buffer
-
-	//Calculate the illumination information for the vertex pixel
-	p.pos3d = v.position;
-*/
-	/*
-	vec3 r = lightPos - v.position;
-	float sphereArea = 4.0f * glm::pi<float>() * glm::dot(r, r);
-	vec3 rNorm = glm::normalize(r);
-	vec3 D = (lightPower * glm::max(glm::dot(rNorm, v.normal), 0.0f)) / sphereArea;
-	p.illumination = v.reflectance * (D + indirectLightPowerPerArea);
-	*/
-/*
-}
-*/
-
-/*
-void PixelShader(const Pixel& p){
-	int x = p.x;
-	int y = p.y;
-
-	if(x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) return;
-
-	if(depthBuffer[y][x] < p.zinv){
-		depthBuffer[y][x] = p.zinv;
-
-		vec3 r = lightPos - p.pos3d;
-		float sphereArea = 4.0f * glm::pi<float>() * glm::dot(r, r);
-		vec3 rNorm = glm::normalize(r);
-		vec3 D = (lightPower * glm::max(glm::dot(rNorm, currentNormal), 0.0f)) / sphereArea;
-		vec3 illumination = currentReflectance * (D + indirectLightPowerPerArea);
-
-		//PutPixelSDL(screen, x, y, illumination);
-	}
-}
-
-void DrawLineSDL(SDL_Surface* surface, ivec2 a, ivec2 b, vec3 color)
-{
-	ivec2 delta = glm::abs(a - b);
-	int pixels = glm::max(delta.x, delta.y) + 1;
-	vector<ivec2> line(pixels);
-	Interpolate(a, b, line);
-	for (int i = 0; i < line.size(); i++) PutPixelSDL(surface, line[i].x, line[i].y, color);
-}
-
-void DrawPolygonEdges(const vector<Vertex>& vertices)
-{
-	int V = vertices.size();
-	// Transform each vertex from 3D world position to 2D image position:
-	vector<Pixel> projectedVertices(V);
-	for (int i = 0; i < V; ++i)
-	{
-		VertexShader(vertices[i], projectedVertices[i]);
-	}
-	// Loop over all vertices and draw the edge from it to the next vertex:
-	for (int i = 0; i < V; ++i)
-	{
-		int j = (i + 1) % V; // The next vertex
-		vec3 color(1, 1, 1);
-		
-		ivec2 first(projectedVertices[i].x, projectedVertices[i].y);
-		ivec2 second(projectedVertices[j].x, projectedVertices[j].y);
-
-		DrawLineSDL(screen, first, second, color);
-	}
-}
-*/
-
 void Draw(){
-	/*SDL_FillRect(screen, 0, 0);
-	for(int y = 0; y < SCREEN_HEIGHT; y++)
-		for(int x = 0; x < SCREEN_WIDTH; x++) depthBuffer[y][x] = 0;
-
-	if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
-
-	for (int i = 0; i < triangles.size(); ++i)
-	{
-		//Update the draw colour
-		//currentColor = triangles[i].color;
-		currentNormal = triangles[i].normal;
-		currentReflectance = triangles[i].color;
-
-		vector<Vertex> vertices(3);
-		vertices[0].position = triangles[i].v[0];
-		//vertices[0].normal = triangles[i].normal;
-		//vertices[0].reflectance = triangles[i].color;
-
-		vertices[1].position = triangles[i].v[1];
-		//vertices[1].normal = triangles[i].normal;
-		//vertices[1].reflectance = triangles[i].color;
-
-		vertices[2].position = triangles[i].v[2];
-		//vertices[2].normal = triangles[i].normal;
-		//vertices[2].reflectance = triangles[i].color;
-
-		DrawPolygon(vertices);
-	}
-	if (SDL_MUSTLOCK(screen))
-		SDL_UnlockSurface(screen);
-	SDL_UpdateRect(screen, 0, 0, 0, 0);*/
 
 	glClearColor(0.0f, 0.0f, 0.0f, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -573,13 +330,19 @@ void Draw(){
 
 	//Render
 	glBindVertexArray(VAO);
-	int currObjOffset = 0;
+	/*int currObjOffset = 0;
 	for (int i = 0; i < objects.size(); i++)
 	{
 		int numtri = objects[i].triangles.size() * 3;
 		updateShaders(mat4(1) * glm::rotate(mat4(1), 180.0f, vec3(1, 0, 0)), objects[i].colour); //TODO: By copy or by reference?
 		glDrawArrays(GL_TRIANGLES, currObjOffset, numtri);
 		currObjOffset += numtri;
+	}*/
+	for (int i = 0; i < boids.size(); i++)
+	{
+		int numtri = boids[i].mesh->triangles.size() * 3;
+		updateShaders(boids[i].getModel(), boids[i].mesh->colour); //TODO: By copy or by reference?
+		glDrawArrays(GL_TRIANGLES, boids[i].mesh->offset, numtri);
 	}
 	
 
